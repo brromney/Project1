af6dc252cccad9395f922b37254ff34ecdd0930c
brromney <blakerromney@gmail.com>
2023-01-31 12:48:05 -0700

    working on string function

--- Scanner.cpp
+++ Scanner.cpp
@@ -5,140 +5,162 @@
 #include "Scanner.h"
 bool loopLine = true;
 Token Scanner::scanToken() {
-    if(input.empty()){
+    if (input.empty()) {
         loopLine = false;
     }
     type = ENDOFFILE;
     value = "EOF";
     line = currLine;
     input = input;
-    while(!input.empty()) {
+    while (!input.empty()) {
         while (isspace(input.front())) {
+            if (input.front() == '\n') {
+                currLine++;
+            }
             input = input.substr(1);
         }
-        if ((input.front() =='\n') || (input.front() =='\t') || (input.front() ==' ') || (input.empty())) {
+        /*if ((input.front() =='\n') || (input.front() =='\t') || (input.front() ==' ') || (input.empty())) {
             loopLine = false;
             break;
         }
-        else if (input.front() == ',') {
+        else*/ if (input.front() == ',') {
             type = COMMA;
             value = ",";
             line = currLine;
             input = input.substr(1);
-            if(input.empty()){loopLine = false;}
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if (input.front() == '.') {
-            type =  PERIOD;
+        } else if (input.front() == '.') {
+            type = PERIOD;
             value = ".";
             line = currLine;
             input = input.substr(1);
-            if(input.empty()){loopLine = false;}
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if (input.front() == '?') {
+        } else if (input.front() == '?') {
             type = Q_MARK;
             value = "?";
             line = currLine;
             input = input.substr(1);
-            if(input.empty()){loopLine = false;}
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if (input.front() == '(') {
+        } else if (input.front() == '(') {
             type = LEFT_PAREN;
             value = "(";
             line = currLine;
             input = input.substr(1);
-            if(input.empty()){loopLine = false;}
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if (input.front() == ')') {
+        } else if (input.front() == ')') {
             type = RIGHT_PAREN;
             value = ")";
             line = currLine;
             input = input.substr(1);
-            if(input.empty()){loopLine = false;}
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if (input.front() == ':' && input[1] == '-') {
+        } else if (input.front() == ':' && input[1] == '-') {
             type = COLON_DASH;
             value = ":-";
             line = currLine;
             input = input.substr(2);
-            if(input.empty()){loopLine = false;}
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if (input.front() == ':') {
+        } else if (input.front() == ':') {
             type = COLON;
             value = ":";
             line = currLine;
             input = input.substr(1);
-            if(input.empty()){loopLine = false;}
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if (input.front() == '*') {
+        } else if (input.front() == '*') {
             type = MULTIPLY;
             value = "*";
             line = currLine;
             input = input.substr(1);
-            if(input.empty()){loopLine = false;}
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if (input.front() == '+') {
+        } else if (input.front() == '+') {
             type = ADD;
             value = "+";
             line = currLine;
             input = input.substr(1);
-            if(input.empty()){loopLine = false;}
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if (input.front() == 'S' && input[1] == 'c' && input[2] == 'h' && input[3] == 'e' && input[4] == 'm' && input[5] == 'e' && input[6] == 's') {
+        } else if (input.front() == 'S' && input[1] == 'c' && input[2] == 'h' && input[3] == 'e' && input[4] == 'm' &&
+                   input[5] == 'e' && input[6] == 's') {
             type = SCHEMES;
             value = "SCHEMES";
             line = currLine;
-            input = input.substr(input.length());
-            if(input.empty()){loopLine = false;}
+            input = input.substr(7);
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if (input.front() == 'F' && input[1] == 'a' && input[2] == 'c' && input[3] == 't' && input[4] == 's' && (input[5] == ' ' || input[5] == ':'|| input[5] == '\n')) {
+        } else if (input.front() == 'F' && input[1] == 'a' && input[2] == 'c' && input[3] == 't' && input[4] == 's' &&
+                   (input[5] == ' ' || input[5] == ':' || input[5] == '\n')) {
             type = FACTS;
             value = "FACTS";
             line = currLine;
             input = input.substr(5);
-            if(input.empty()){loopLine = false;}
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if (input.front() == 'Q' && input[1] == 'u' && input[2] == 'e' && input[3] == 'r' && input[4] == 'i' && input[5] == 'e' && input[6] == 's') {
+        } else if (input.front() == 'Q' && input[1] == 'u' && input[2] == 'e' && input[3] == 'r' && input[4] == 'i' &&
+                   input[5] == 'e' && input[6] == 's') {
             type = QUERIES;
             value = "QUERIES";
             line = currLine;
-            input = input.substr(input.length());
-            if(input.empty()){loopLine = false;}
+            input = input.substr(7);
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-
-        else if (input.front() == '#') {
+        } else if (input.front() == '#') {
+            int i = 0;//COMMENT
+            string outString;
+            while (input[i] != '\n') {
+                outString += input[i];
+                i++;
+            }
             type = COMMENT;
-            value = input; //TODO print whole line
+            value = outString;
             line = currLine;
-            input = input.substr(input.length());
-            if(input.empty()){loopLine = false;}
+            input = input.substr(i);
+            if (input.empty()) { loopLine = false; }
             break;
         }
-        else if ((input.front() && input[(input.length() - 1)] == '\'') || (input.front() && input[(input.length() - 1)] == '"') && (input.front() == input[input.length()-1])) {
+        else if (input.front() == '\'') {
+            int i = 1;//STRING
+            string outString;
+            outString = outString + '\'';
+            do {
+                outString += input[i];
+                i++;
+            } while (input[i] != '\'');
+
+            /*if(input.empty()){
+                type = UNDEFINED;
+                value = outString;
+                line = currLine;
+                input = input.substr(input.length());
+                if(input.empty()){loopLine = false;}
+                break;
+            }*/
             type = STRING;
-            value = input;
+            value = outString;
             line = currLine;
-            input = input.substr(input.length());
-            if(input.empty()){loopLine = false;}
+            input = input.substr(i);
+            if (input.empty()) { loopLine = false; }
             break;
-        }
-        else if ((input.front() && input[(input.length() - 1)] != '\'') && (input.front() && input[(input.length() - 1)] != '"')) {
+        } else if ((input.front() != '\'')) {
+            int i = 0;
+            string outString;
+            while (input[i] != '\'') {
+                if (!isspace(input[i])) {
+                    outString += input[i];
+                    i++;
+                }
+            }
             type = ID;
             value = input;
             line = currLine;
             input = input.substr(input.length());
-            if(input.empty()){loopLine = false;}
+            if (input.empty()) { loopLine = false; }
             break;
         }
         /*else if (input.empty()) { //fixme
@@ -149,14 +171,15 @@ Token Scanner::scanToken() {
             if(input.empty()){loopLine = false;}
             break;
         }*/
-        else {
+        /*else {
             type = UNDEFINED;
             value = input;
             line = currLine;
             input = input.substr(input.length());
             if(input.empty()){loopLine = false;}
             break;
-        }
+        }*/
+
+        return Token(type, value, line);
     }
-    return Token(type, value, line);
-}
+};
--- main.cpp
+++ main.cpp
@@ -4,23 +4,25 @@
 using namespace std;
 
 int main() {
-    ifstream infile;
-    infile.open(R"(C:\Users\blake\Documents\BYU\CS236\Project 1\noErrors.txt)");
+    ifstream infile(R"(C:\Users\blake\Documents\BYU\CS236\Project 1\emptyQuote.txt)");
     string fileString;
+    string fileContent;
     if(!infile.is_open()){
         cerr << "Could not open/find file" << endl;
     }
     else if(infile.is_open()){
-        while(!infile.eof()){
-            getline(infile, fileString);
-            cout << fileString << endl;
-            Scanner s = Scanner(fileString);
-            while(loopLine) {
-                Token t = s.scanToken();
-                cout << t.typeName() << endl;
-            }
-            loopLine = true;
+        while(getline(infile, fileString)){
+            //cout << fileString << endl;
+            fileContent += fileString + "\n";
         }
+        cout << fileContent << endl;
         infile.close();
+        Scanner s = Scanner(fileContent);
+        while(loopLine) {//maybe s.input. length maybe change loopline to eof checker
+            Token t = s.scanToken();
+            cout << t.typeName() << endl;
+        }
+        loopLine = true;
+
     }
 }
